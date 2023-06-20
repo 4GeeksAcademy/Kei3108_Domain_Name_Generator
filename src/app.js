e0601f3dd1fb742f7cc652f2ad019351969ad751
Kei3108 <128880307+Kei3108@users.noreply.github.com>
2023-06-20 02:40:18 +0000

    Finish

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adjetive.length; j++) {
     for ( let k = 0; k < noun.length; k++) {
       for (let l = 0; l < extension.length; l++) {
-        console.log (pronoun[i] + adjetive[j] + noun[k] + extension[l]);
+        console.log(pronoun[i] + adjetive[j] + noun[k] + extension[l]);
       }  
     }
   } 
